6cd7570e53728e42219756365324e82f30e50b52
Sojan Mathew <sojanvilayilvadakkethil.shajimathew@sjsu.edu>
2019-11-30 19:50:50 -0800

    grpc communicaation implementation

--- replication.py
+++ replication.py
@@ -1,16 +1,23 @@
+import sys
 import random
 import socket
 from threading import Thread
 import time
 import os
 import json
-
+sys.path.append('./proto')
+sys.path.append('./service')
+from concurrent import futures
+from threading import Thread
+import grpc
+import fileService_pb2
+import fileService_pb2_grpc
 
 class Replicate:
     # hold infected nodes
     # initialization method.
     # pass the port of the node and the ports of the nodes connected to it
-    localIP = "169.105.246.9"
+    localIP = "169.105.246.4"
     localPort = 21000
     bufferSize = 1024
     # Create a datagram socket
@@ -31,13 +38,25 @@ class Replicate:
             for line in file:
                 print(line)
                 if hostname in line:
-                    return True
+                    return False
+
 
     def replicateContent(self, hostname, intial_Replicate_Server):
         # logic to pick up bytes from memory and transmit
         bytes_read_from_memory = str.encode("Srinivas")
+        serverAddress = "localhost"
+        serverPort = 9000
+        channel = grpc.insecure_channel(serverAddress + ":" + str(serverPort))
+        replicate_stub = fileService_pb2_grpc.FileserviceStub(channel)
+        request = fileService_pb2.FileData(initialReplicaServer = "initialReplicaServer", message = "message",
+                                                                                vClock = "vClock")
+        resp = replicate_stub.ReplicateFile(request)
+        print(resp)
+        #self.transmit_message(bytes_read_from_memory, intial_Replicate_Server, hostname.decode("utf-8"), False)
 
-        self.transmit_message(bytes_read_from_memory, intial_Replicate_Server, hostname.decode("utf-8"), False)
+    def ReplicateFile(self, request, context):
+        print("request", request.initialReplicaServer )
+        return resources.fileService_pb2.ack(success=True, message="Data Replicated.")
 
 
     def findNeighbors(self, message, intial_Replicate_Server):
@@ -69,25 +88,27 @@ class Replicate:
             print(message)
             print(address[0])
             data = json.loads(message.decode())
-            intialReplicaServer = data.get("initalReplicaServer")
-            print("intialReplicaServer", intialReplicaServer)
+            initialReplicaServer = data.get("initialReplicaServer")
+            print("initialReplicaServer", initialReplicaServer)
             message = data.get("message")
             isFirstServer = data.get("isFirstServer")
             if message=="true":
                 print("Trying to replicate at", address[0])
-                self.replicateContent(intialReplicaServer, address[0])
+                self.replicateContent(initialReplicaServer, address[0])
             elif message.isnumeric() and isFirstServer == True:
-                print("First Server", intialReplicaServer)
-                self.findNeighbors(message, intialReplicaServer)
+                print("First Server", initialReplicaServer)
+                self.findNeighbors(message, initialReplicaServer)
             elif message.isnumeric():
             # Logic to check for write
-                canAccomodate = self.checkforCapacity(message, self.hostname)
+                canAccomodate = self.checkforCapacity(message, self.localIP)
                 if canAccomodate:
                     replicate_true = str.encode("true")
-                    self.transmit_message(replicate_true, intialReplicaServer, address[0].decode("utf-8"), False)
+                    print(address[0])
+                    string = str(address[0])
+                    self.transmit_message(replicate_true, initialReplicaServer, string.decode("utf-8"), False)
                     print("inside if")
                 else:
-                   self.findNeighbors(message, intialReplicaServer)
+                   self.findNeighbors(message, initialReplicaServer)
             else:
                 #logic to write to memory
                 print("Write to memory")
@@ -100,14 +121,15 @@ class Replicate:
         '''bytesToSend = str.encode(data)
         print(bytesToSend)'''
         # logic to chose neighbors and check if neighbor is alive and not in list of already transmitted
-        serverAddressPort = ("169.105.246.4", 21000)
+        serverAddressPort = ("169.105.246.3", 21000)
         bufferSize = 1024
         # Create a UDP socket at client side
         # Send to server using created UDP socket
-        message = json.dumps({"isFirstServer": firstServer, "intialReplicaServer": intial_Replicate_Server, "message": message})
+        message = json.dumps({"isFirstServer": firstServer, "initialReplicaServer": intial_Replicate_Server, "message": message})
         print("Sending message to",message)
         self.UDPServerSocket.sendto(message.encode(), serverAddressPort)
         #time.sleep(2)
 
+
     def start_threads(self):
-        Thread(target=self.receive_message).start()
+        Thread(target=self.replicateContent).start()
---
+++ service/ReplicationService.py
@@ -0,0 +1,12 @@
+import grpc
+import sys
+sys.path.append('./proto')
+sys.path.append('./service')
+import fileService_pb2_grpc
+import fileService_pb2
+
+class ReplicationService(fileService_pb2_grpc.FileserviceServicer):
+
+    def ReplicateFile(self, request, context):
+        print("request", request.initialReplicaServer)
+        return fileService_pb2.ack(success=True, message="Data Replicated.")
