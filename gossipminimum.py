57dfa8cb20af4962b8e53ac258b2cfd38429831a
Vaishali Koul <Koulvaishali07@yahoo.com>
2019-12-08 18:18:40 -0800

    Added code

--- gossipminimum.py
+++ gossipminimum.py
@@ -32,7 +32,7 @@ class GossipProtocol:
     totalNodes = [1234, 3456, 7899, 7543]
     sys.setrecursionlimit(200000)
     localMinimumCapacity = -sys.maxsize - 1
-    IPaddress = "169.105.246.7"
+    IPaddress = "169.105.246.9"
     localPort = 21000
     local_message = None
     bufferSize = 1024
@@ -41,6 +41,8 @@ class GossipProtocol:
     # Bind to address and ip
     UDPServerSocket.bind((IPaddress, localPort))
     blacklisted_nodes = []
+    minimum_IP = None
+    minimum_Capacity = None
 
     def __init__(self):
         self.start_threads()
@@ -80,7 +82,7 @@ class GossipProtocol:
     def updated_message_util(self, data, minimum_capacity, leastUsedIP, gossip_phase):
         # update message
         print("DATA = ", leastUsedIP)
-        Dictionary = {leastUsedIP: minimum_capacity}
+        Dictionary = {self.minimum_IP: self.minimum_Capacity}
         Dict = data.get("Dictionary")
         IPaddress = data.get("IPaddress")
         gossip = gossip_phase
@@ -96,6 +98,8 @@ class GossipProtocol:
         print("SICT = ", dictionary)
         mini = min(dictionary, key=lambda k: dictionary[k])
         print("MINIMUM = ",[mini, dictionary[mini]])
+        self.minimum_IP = mini.strip('\n')
+        self.minimum_Capacity = dictionary[mini]
         return [mini.strip('\n'), dictionary[mini]]
 
     def fetch_all_neighbors(self):
@@ -111,7 +115,7 @@ class GossipProtocol:
 
     def get_minimum_capacity_neighbors(self, initalReplicaServer):
         list_of_neigbors = []
-        capacity_of_neighbors = {}
+        capacity_of_neighbors = {self.IPaddress:7929}
         print("INITIALREPLICA", initalReplicaServer)
         filepath = 'data/neighbors.txt'
         with open(filepath, "r") as ins:
@@ -119,12 +123,15 @@ class GossipProtocol:
                 line = line.strip('\n')
                 list_of_neigbors.append(line)
         counter = 0
+        if initalReplicaServer in list_of_neigbors:
+            list_of_neigbors.remove(initalReplicaServer)
         while len(list_of_neigbors) > 0:
             print("NEIGHBORS = ", list_of_neigbors)
             forwardIP = random.choice(list_of_neigbors)
             print("SENDING NEXT TO = ", forwardIP)
             hostname = str.encode(forwardIP)
             hostname2 = forwardIP
+
             print("HOSTNAME = ", hostname2, "INT REPL =", initalReplicaServer)
             if hostname2 != initalReplicaServer:
                 print("ping -c 1 " + hostname.decode("utf-8"))
@@ -138,7 +145,7 @@ class GossipProtocol:
                     elif hostname2 == "169.105.246.6":
                         coordinates = "(1,1)"
                     else:
-                        coordinates = "(0,0)"
+                        coordinates = "(-1,0)"
 
                     print("GET COORDINATES CAP OF", coordinates)
                     IPaddress,capacity = self.getneighborcapacity(coordinates)
@@ -150,15 +157,11 @@ class GossipProtocol:
                     print(hostname, 'down')
                     counter += 1
                     list_of_neigbors.remove(hostname2)
-            else:
-                list_of_neigbors.remove(hostname2)
-                return [initalReplicaServer, 7929]
-                #continue
 
         if len(capacity_of_neighbors) == 0:
             return None
         else:
-            print("C O N",capacity_of_neighbors)
+            print("capacity_of_neighbors = ",capacity_of_neighbors)
             first_minimum = self.find_minimum_in_dictionary(capacity_of_neighbors)
             return [first_minimum[0], first_minimum[1]]
 
@@ -169,6 +172,8 @@ class GossipProtocol:
             print("GOT DATA ", data, " FROM", address[0])
             IPaddress = data.get("IPaddress")
             gossip_flag = data.get("gossip")
+            Dictionary = data.get("Dictionary")
+            BlackListedNodes = data.get("BlackListedNodes")
             Convergence_Value = self.checkforConvergence(data)
             print("LATEST VALUES", IPaddress, gossip_flag, Convergence_Value)
             if str(IPaddress) == "169.105.246.9" and gossip_flag == False:
@@ -201,6 +206,7 @@ class GossipProtocol:
                 print("IN ELIF")
                 list_of_neighbors = self.fetch_all_neighbors()
                 minimum_capacity_neighbor = self.get_minimum_capacity_neighbors(IPaddress)
+                print("MINIMUM of minimum_capacity_neighbor", minimum_capacity_neighbor)
                 if minimum_capacity_neighbor != None:
                     dict = data.get("Dictionary")
                     received_minimum_capacity = dict[list(dict.keys())[0]]
@@ -209,7 +215,8 @@ class GossipProtocol:
                     print("------------", minimum_capacity, minimum_capacity_neighbor[0], "------------")
                     print("minimum_capacity_neighbor",minimum_capacity_neighbor[0])
                     print("received_minimum_capacity", received_minimum_capacity)
-                    IPaddress, gossip, Dictionary, BlackListedNodes= self.updated_message_util(data, minimum_capacity, minimum_capacity_neighbor[0], True)
+                    if received_minimum_capacity != minimum_capacity
+                        IPaddress, gossip, Dictionary, BlackListedNodes= self.updated_message_util(data, minimum_capacity, minimum_capacity_neighbor[0], True)
                     for ip in range(len(list_of_neighbors)):
                         response = os.system("ping -c 1 " + list_of_neighbors[ip].strip('\n'))
                         if response == 0:
